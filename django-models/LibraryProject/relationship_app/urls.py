cd4afa7baa5fed23e940230e5de36fe2e374a114
Your Name <your-email@example.com>
2025-08-01 15:47:06 +0300

    Corrected urls.py to match checker requirements

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, register
+from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('', list_books, name='home'),
-    path('books/', list_books, name='list_books'),
-    path('library_detail/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('', views.list_books, name='home'),
+    path('books/', views.list_books, name='list_books'),
+    path('library_detail/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+
+    # New URL patterns for authentication
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
-    path('register/', register, name='register'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html', next_page='login'), name='logout'),
+    path('register/', views.register, name='register'),
 ]
