36ac2dd2adfc11a15660ca52938b7892c2811725
Your Name <your-email@example.com>
2025-08-04 17:45:24 +0300

    feat: Configure and customize Django admin for Book model

--- Introduction_to_Django/bookshelf/admin.py
+++ Introduction_to_Django/bookshelf/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
+# bookshelf/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Book
+
+# Create a custom Admin class for the Book model
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    list_filter = ('author', 'publication_year')
+    search_fields = ('title', 'author')
+
+# Register the Book model with the custom Admin class
+admin.site.register(Book, BookAdmin)
