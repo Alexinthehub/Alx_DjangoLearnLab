6ad25d5066e67f09c929270bdcbc002b56da3b32
Your Name <your-email@example.com>
2025-08-13 18:02:58 +0300

    fix: api/views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 # This view is a simple ListView
 class BookListView(generics.ListAPIView):
