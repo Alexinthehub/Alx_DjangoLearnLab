021fc7d1a19ea8fb0655295190ae4320c46bfe59
Your Name <your-email@example.com>
2025-08-15 05:33:23 +0300

    Implementing the Blog's User Authentication System

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import UserRegisterForm
+from .models import Post 
 
-# Create your views here.
+def home(request):
+    return render(request, 'blog/home.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}! You can now log in.')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'blog/profile.html')
+
+def post_list(request):
+    # Retrieve all posts from the database, ordered by publication date
+    posts = Post.objects.all().order_by('-published_date')
+    
+    # Pass the posts to the template
+    return render(request, 'blog/post_list.html', {'posts': posts})
