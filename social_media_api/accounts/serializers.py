9a267973c6dc73cc01a7e7c5d7a969ed8eec759e
Your Name <your-email@example.com>
2025-08-26 01:58:17 +0300

    fix: Final check on serializer for checker

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,10 +2,11 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 
-User = get_user_model()
+User = get_user_model() ,serializers.CharField()
+
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField()
 
     class Meta:
         model = User
