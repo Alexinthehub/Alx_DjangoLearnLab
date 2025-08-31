44815e8f5b112d3a82f9b128655da6894910a76a
Your Name <your-email@example.com>
2025-08-31 11:33:04 +0300

    Prepared for PythonAnywhere deployment

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -1,8 +1,6 @@
-"""
-URL configuration for social_media_api project.
-
+"""social_media_api URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,13 +12,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# social_media_api/urls.py
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', include('accounts.urls')),
-    path('api/', include('posts.urls')),
-    path('api/', include('notifications.urls')), # Add this line
+    path('api/', include('api.urls')),
 ]
